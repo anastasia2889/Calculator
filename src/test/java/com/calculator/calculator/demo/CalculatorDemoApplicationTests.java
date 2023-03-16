c9f7ff7609d13266c5cc3e1011db75a330f2a296
anastasia2889 <anastasia.brodskaja@yandex.ru>
2023-03-16 19:34:48 +0300

    initial commit

---
+++ src/test/java/com/calculator/calculator/demo/CalculatorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculator.calculator.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
